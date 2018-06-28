158be3d9c488fce5b62389ca9fa339877893dac1
poppy <siripen7766@gmail.com>
2018-06-28 12:12:39 +0700

    Before Lunch

--- app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/RegisterFragment.java
@@ -10,21 +10,13 @@ import android.view.ViewGroup;
 
 import masterung.androidthai.in.th.srrufriend.R;
 
-public class MainFragment extends Fragment{
+public class RegisterFragment  extends Fragment{
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
- //       Register Controler
-
-    } //Main Methot
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
     }
-
-} //Main Class
+}
